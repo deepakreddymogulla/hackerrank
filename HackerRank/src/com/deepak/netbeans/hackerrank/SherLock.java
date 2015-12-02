b8366eae85f15325da79cff2b491d7fc313bac26
Deepak Reddy Mogulla <deepak.reddy9999@gmail.com>
2015-12-02 00:10:18 -0700

    Solution to Sherlock and The Beast problem

--- HackerRank/src/com/deepak/netbeans/hackerrank/SherLock.java
+++ HackerRank/src/com/deepak/netbeans/hackerrank/SherLock.java
@@ -6,8 +6,6 @@ package com.deepak.netbeans.hackerrank;
 
 import java.util.Scanner;
 
-        //throw new UnsupportedOperationException("Not yet implemented");
-    
 /**
  *
  * @author ghm8mog
@@ -19,72 +17,44 @@ public class SherLock {
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
         
+        //Start loop to read ints
         for(int i=1; i<=num; i++){
             //System.out.println("i:"+i);
             int k = in.nextInt();
             
-            if (k==3 || k==5 || k>=8){
-                getCode(k);
-            } else System.out.println("-1");
-            
-            
+            // Flow of possible cases
+            switch (k) {
+                case 0: System.out.print("-1"); break;
+                case 1: System.out.print("-1"); break;
+                case 2: System.out.print("-1"); break;
+                case 4: System.out.print("-1"); break;
+                case 7: System.out.print("-1"); break;
+                default: analyzeK(k);
+            }
+            //
+            System.out.println();
         } 
+        //
    }
-
-    private static void getCode(int k) {
-        
-        switch(k){
-            case 3: 
-                System.out.println("555");
-                break;
-            case 5:
-                System.out.println("33333");
-                break;
-            default:
-                if (k>=8) {
-                int k1 = k/8;
-                int k2 = k%8;
-                getActualCode(k1, k2);
+    
+    private static void analyzeK(int k) {
+        // Printing the code
+        int z = k%3;
+        if ((k%3) == 0) {
+            for (int i=1; i<=k/3; i++) {
+            System.out.print("555");
             }
-        }
-        
-        //throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    private static void getActualCode(int k1, int k2) {
-        String out = "";
-        
-        if(k2==0 || (k2%3)==0 || (k2%5)==0){
-            if(k2==0){
-                getRoutine(k1, out);
-            } else if(k2%3==0){
-                for(int i5=1; i5<=(k2/3); i5++){
-                        out = out+"555";
-                    }
-                getRoutine(k1, out);
-            } else {
-                for(int i3=1; i3<=(k2/5); i3++){
-                        out = out+"33333";
-                    }
-                getRoutine(k1, out);
-            }
-            
         } else {
-            getRoutine(0, "-1");
+            int n = (3-z)*5;
+            k = k-n;
+            for (int i=1; i<=k/3; i++) {
+                System.out.print("555");
+            }
+            for (int i=1; i<=n/5; i++) {
+                System.out.print("33333");
+            }
         }
-    }
-
-    private static void getRoutine(int k1, String out) {
-        
-        for(int i5=1; i5<=k1; i5++){
-            out = "555"+out;
-        }
-        for(int i3=1; i3<=k1; i3++){
-            out = out+"33333";
-        }
-        
-        System.out.println(out);
+        //
         //throw new UnsupportedOperationException("Not yet implemented");
     }
-
 }
